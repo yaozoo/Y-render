5f8bbfe1b16de0a0624e2d937dd424208a5e70f3
luyao <luyao@doublefs.com>
2023-02-06 11:35:24 +0800

    新增下载vue功能

--- src/views/renderForm/component/top/viewVUE.vue
+++ src/views/renderForm/component/top/viewVUE.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: luyao
  * @Date: 2022-08-23 15:11:36
- * @LastEditTime: 2022-11-19 15:13:37
+ * @LastEditTime: 2023-02-06 11:32:38
  * @Description: 
  * @LastEditors: luyao
  * @FilePath: /Y-render/src/views/renderForm/component/top/viewVUE.vue
@@ -25,6 +25,19 @@
       type="success"
       >COPY</el-button
     >
+
+    <el-button
+      style="
+        height: 28px;
+        margin-bottom: 10px;
+        width: 100px;
+        position: absolute;
+        left: 120px;
+      "
+      @click="downLoadVue(code, `${Math.floor(Math.random() * 100000)}.vue`)"
+      type="success"
+      >下载VUE文件</el-button
+    >
     <!-- @click="copyText(JSON.parse(new Function('return ' + code)()))" -->
     <highlightjs autodetect :code="code" />
   </el-dialog>
@@ -32,8 +45,9 @@
 
 <script setup lang="ts">
 import { inject, ref, watch } from "vue";
-import { cloneDeep } from "lodash";
-import { ElMessage } from "element-plus";
+import { cloneDeep, parseInt } from "lodash";
+import { ElMessage, ElMessageBox } from "element-plus";
+import { saveAs } from "file-saver";
 
 let dialogVisible = ref(false);
 defineExpose({ showFun });
@@ -81,4 +95,35 @@ const copyText = (text: any, time = 400) => {
     timer = null;
   }, time);
 };
+
+function downLoadVue(val: any, defaultFileName: any) {
+  ElMessageBox.prompt("请输入文件名", "Tip", {
+    confirmButtonText: "确认",
+    cancelButtonText: "取消",
+    inputValue: defaultFileName,
+  })
+    .then(({ value }) => {
+      if (!value) {
+        value = defaultFileName;
+      }
+
+      if (!defaultFileName) {
+        ElMessage({
+          type: "info",
+          message: "请输入文件名",
+        });
+        return false;
+      }
+      const fileBlob = new Blob([val], {
+        type: "text/plain;charset=utf-8",
+      });
+      saveAs(fileBlob, value);
+    })
+    .catch(() => {
+      ElMessage({
+        type: "info",
+        message: "Input canceled",
+      });
+    });
+}
 </script>
